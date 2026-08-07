1df9f2b3073ffa71fe1488ebe3afe31ca5be1dc7
Sapon Sarub <saponsarub@gmail.com>
2026-08-07 10:05:47 +0700

    edit1

--- Sql/SalesData.sql
+++ Sql/SalesData.sql
@@ -2,7 +2,7 @@ SELECT
     YEAR(si.InvoiceDate) AS InvoiceYear,
     si.CustomerAccount,
     si.CustomerName,
-    COUNT(si.LineAmount) AS CountSell
+    sum(si.LineAmount) AS CountSell
 FROM syndpdev001.dbo.tb_SalesInvoice si
 WHERE si.SysCompanyId = 'DRPH'
 GROUP BY 
